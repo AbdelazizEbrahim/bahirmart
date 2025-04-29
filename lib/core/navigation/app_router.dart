1f49945be92f19f60ec56218546e1198d8f17b59
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2025-04-29 09:33:11 +0300

    Auction and auction detail pages

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:bahirmart/pages/landing_page.dart';
 import 'package:bahirmart/pages/products_page.dart';
 import 'package:bahirmart/pages/product_detail_page.dart';
+import 'package:bahirmart/pages/auctions_page.dart';
 import 'package:bahirmart/core/models/product_model.dart' as prod_model;
 
 class AppRouter {
@@ -32,7 +33,7 @@ class AppRouter {
         );
       case '/auctions':
         return MaterialPageRoute(
-            builder: (_) => const Scaffold(body: Center(child: Text('Auctions Page'))));
+            builder: (_) => const AuctionListPage());
       case '/cart':
         return MaterialPageRoute(
             builder: (_) => const Scaffold(body: Center(child: Text('Cart Page'))));
